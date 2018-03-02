d7fcc8fb4b441d6648ace2b588c2038bac5132f9
dwqs <461147874@qq.com>
2018-03-02 15:58:17 +0800

    refactor: Maybe support custom event

---
+++ src/custom-event.js
@@ -0,0 +1,13 @@
+// CustomEvent: https://caniuse.com/#search=CustomEvent
+function createCustomEvent (eventName) {
+    if (typeof window.CustomEvent === 'function') {
+        return new CustomEvent(eventName, {
+            bubbles: false,
+            cancelable: false,
+            detail: null
+        });
+    } else {
+        const evt = document.createEvent('CustomEvent');
+        evt.initCustomEvent(eventName, false, false, null);
+    }
+};
