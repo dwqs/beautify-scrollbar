a5e3fefa4ba659a45ddc902581fbb9b5af77ec3c
dwqs <461147874@qq.com>
2018-03-03 00:07:18 +0800

    refactor: Add examples

---
+++ examples/container.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container" id="container">
+        <div class="content">
+            <img src="http://img02.tooopen.com/images/20160427/tooopen_sy_160785253195.jpg" alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+    import BeautifyScrollbar from '../src/index';
+    export default {
+        mounted () {
+            this.bs = new BeautifyScrollbar('#container')
+        }
+    }
+</script>
+
+<style lang="less">
+    .container {
+        width: 600px;
+        height: 500px;
+        box-sizing: border-box;
+        border: 1px solid #ccc;
+        .content {
+            // background-image: url(http://img02.tooopen.com/images/20160427/tooopen_sy_160785253195.jpg);
+            // background-repeat: no-repeat;
+            width: 1024px;
+            height: 694px;
+            img {
+                width: 100%;
+                height: 100%;
+            }
+        }
+    }
+</style>
---
+++ examples/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+
+import App from './app';
+
+/* eslint-disable */
+new Vue({
+    el: '#app',
+    render: (h) => h(App)
+});
