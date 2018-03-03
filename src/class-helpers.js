d4fab5ee6f184235ee76f8d62bab76998a9d4ed5
dwqs <461147874@qq.com>
2018-03-03 20:25:55 +0800

    feat: Add class helpers

---
+++ src/class-helpers.js
@@ -0,0 +1,60 @@
+function toArray (element) {
+    let str = (element.getAttribute('class') || '').replace(/^\s+|\s+$/g, '');
+    let classes = str.split(/\s+/);
+    if (classes[0] === '') {
+        classes.shift();
+    }
+    return classes;
+};
+
+function removeClass (element, cls) {
+    if (!cls || typeof cls !== 'string') {
+        return void 0;
+    }
+    let _curClasses = [];
+    let curClasses = toArray(element);
+    let classes = cls.split(' ');
+
+    for (let i = 0; i < classes.length; i++) {
+        let clsName = classes[i];
+        if (!clsName) {
+            continue;
+        }
+        if (element.classList) {
+            element.classList.remove(clsName);
+        } else {
+            curClasses.splice(curClasses.indexOf(clsName), 1);
+        }
+    }
+    _curClasses = curClasses;
+
+    if (!element.classList) {
+        element.setAttribute('class', _curClasses.join(' '));
+    }
+}
+
+function addClass (element, cls) {
+    let curClasses = toArray(element);
+    let classes = cls.split(' ');
+
+    for (let i = 0; i < classes.length; i++) {
+        let clsName = classes[i];
+        if (!clsName) {
+            continue;
+        }
+        if (element.classList) {
+            element.classList.add(clsName);
+        } else {
+            curClasses.push(clsName);
+        }
+    }
+
+    if (!element.classList) {
+        element.setAttribute('class', curClasses.join(' '));
+    }
+}
+
+export {
+    addClass,
+    removeClass
+};
