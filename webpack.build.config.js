dd3f6b22fb67c5314bcb71e943f70e9504ac82de
dwqs <461147874@qq.com>
2018-03-04 14:28:42 +0800

    refactor: Extract css

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -3,6 +3,8 @@ const webpack = require('webpack');
 const os = require('os');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -26,11 +28,26 @@ module.exports = {
             {
                 test: /\.less$/,
                 type: 'javascript/auto',
-                use: ['css-loader', 'postcss-loader', 'less-loader']
+                use: ExtractTextPlugin.extract({
+                    fallback: 'css-loader',
+                    use: ['postcss-loader', 'less-loader']
+                })
             }
         ]
     },
     plugins: [
+        new ExtractTextPlugin({
+            filename: '[name].css'
+        }),
+
+        new OptimizeCSSPlugin({
+            cssProcessorOptions: {
+                safe: true
+            },
+            cssProcessor: require('cssnano'),
+            assetNameRegExp: /\.less|\.css$/g
+        }),
+
         new UglifyJsPlugin({
             parallel: true,
             cache: '.cache/',
